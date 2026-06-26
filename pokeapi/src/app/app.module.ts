fed96a8f78297d0edf1fc9499b24e55b3ef18c54
jeidy2910 <jeidyc33@gmail.com>
2026-06-26 06:48:17 -0500

    feat: Se realiza codigo en archivos

--- pokeapi/src/app/app.module.ts
+++ pokeapi/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule // Importante para las peticiones HTTP
   ],
   providers: [],
   bootstrap: [AppComponent]
